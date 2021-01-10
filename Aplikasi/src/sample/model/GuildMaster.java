71f0a8112bf84a0ba0261505aad5807cc884d2c7
FadelNurAkhmad <76637105+FadelNurAkhmad@users.noreply.github.com>
2021-01-11 00:10:51 +0700

    Update GuildMaster.java

--- Aplikasi/src/sample/model/GuildMaster.java
+++ Aplikasi/src/sample/model/GuildMaster.java
@@ -1,5 +1,25 @@
 package sample.model;
 
-public class GuildMaster {
+import javax.swing.text.TableView;
 
+public class GuildMaster extends User implements Management {
+    //constructor
+    public GuildMaster() { }
+    public GuildMaster(String username, String password) { super(username, password); }
+
+
+    @Override
+    public void create(TableView tableView, Object object, MANAGE mode) { }
+
+    @Override
+    public void read(TableView tableView, MANAGE mode) { }
+
+    @Override
+    public void update(TableView tableView, Object object, MANAGE mode) { }
+
+    @Override
+    public void delete(TableView tableView, MANAGE mode) { }
+
+    @Override
+    public Object get(TableView tableView) { }
 }
