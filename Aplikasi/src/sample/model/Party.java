7cdeda786721f9cc1317efde1bb482348a701937
FadelNurAkhmad <76637105+FadelNurAkhmad@users.noreply.github.com>
2021-01-11 00:11:42 +0700

    Update Party.java

--- Aplikasi/src/sample/model/Party.java
+++ Aplikasi/src/sample/model/Party.java
@@ -1,4 +1,12 @@
 package sample.model;
 
+import java.util.ArrayList;
+
 public class Party {
+    private String name;
+    private String rank;
+    private ArrayList<Hunter> members;
+    private int power;
+    private ArrayList<Quest> quest;
+
 }
